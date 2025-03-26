fcfe399d35850c9cc984856061d390d88e0ff6cd
Nursat8 <kulumkanovnursat@gmail.com>
2025-03-26 09:33:03 +0100

    Update Exclusion Code for Coal.py

--- Exclusion Code for Coal.py
+++ Exclusion Code for Coal.py
@@ -312,31 +312,31 @@ def main():
 
     # Mining
     with st.sidebar.expander("Mining", expanded=True):
-        ur_mining_checkbox = st.checkbox("Urgewald: Exclude if thermal coal revenue > threshold", value=False)
-        ur_mining_threshold = st.number_input("UR coal revenue threshold (%)", value=5.0)
-        sp_mining_checkbox = st.checkbox("S&P: Exclude if thermal coal revenue > threshold", value=True)
-        sp_mining_threshold = st.number_input("S&P Mining Threshold (%)", value=5.0)
-        exclude_mt = st.checkbox("Exclude if > MT threshold", value=True)
+        sp_mining_checkbox = st.checkbox("S&P: Exclude if thermal coal revenue > threshold (mining)", value=True)
+        sp_mining_threshold = st.number_input("S&P Mining Threshold (%)", value=15.0)
+        ur_mining_checkbox = st.checkbox("Urgewald: Exclude if thermal coal revenue > threshold (mining)", value=True)
+        ur_mining_threshold = st.number_input("UR Mining: Level 1 threshold (%)", value=5.0)
+        exclude_mt = st.checkbox("Exclude if >10MT indicated", value=True)
         mt_threshold = st.number_input("Max production (MT) threshold", value=10.0)
 
     # Power
     with st.sidebar.expander("Power", expanded=True):
-        ur_power_checkbox = st.checkbox("Urgewald: Exclude if thermal coal revenue > threshold", value=False)
-        ur_power_threshold = st.number_input("UR Power: Level 1 threshold (%)", value=20.0)
-        sp_power_checkbox = st.checkbox("S&P: Exclude if thermal coal revenue > threshold", value=True)
+        sp_power_checkbox = st.checkbox("S&P: Exclude if thermal coal revenue > threshold (power)", value=True)
         sp_power_threshold = st.number_input("S&P Power Threshold (%)", value=20.0)
+        ur_power_checkbox = st.checkbox("Urgewald: Exclude if thermal coal revenue > threshold (power)", value=True)
+        ur_power_threshold = st.number_input("UR Power: Level 1 threshold (%)", value=20.0)
         exclude_power_prod = st.checkbox("Exclude if > % production threshold", value=True)
         power_prod_threshold = st.number_input("Max coal power production (%)", value=20.0)
         exclude_capacity = st.checkbox("Exclude if > capacity (MW) threshold", value=True)
         capacity_threshold = st.number_input("Max installed capacity (MW)", value=10000.0)
 
     # UR Exclusion Level 2
-    with st.sidebar.expander("UR Exclusion Level 2", expanded=False):
-        ur_level2_checkbox = st.checkbox("Apply UR Level 2 exclusion", value=False)
-        ur_level2_threshold = st.number_input("UR Level 2 revenue threshold (%)", value=10.0)
+    with st.sidebar.expander("UR Exclusion Level 2", expanded=True):
+        ur_level2_checkbox = st.checkbox("Apply UR Level 2 exclusion", value=True)
+        ur_level2_threshold = st.number_input("UR Level 2 revenue threshold (%)", value=6.0)
 
     # Expansion
-    with st.sidebar.expander("Exclude if expansion plans on business", expanded=False):
+    with st.sidebar.expander("Expansion", expanded=False):
         expansions_possible = ["mining","infrastructure","power","subsidiary of a coal developer"]
         expansion_exclude = st.multiselect("Exclude if expansion text contains any of these", expansions_possible, default=[])
 
