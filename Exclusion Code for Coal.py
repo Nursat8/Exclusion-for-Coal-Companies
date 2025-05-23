b78989eded868d3ebd09e80888d479ee2a52c3ee
Nursat Kulumkanov <kulumkanovnursat@gmail.com>
2025-05-23 16:27:14 +0200

    Update Exclusion Code for Coal.py

--- Exclusion Code for Coal.py
+++ Exclusion Code for Coal.py
@@ -380,8 +380,14 @@ def main():
         sp_merged   = merged_sp[ merged_sp["Merged"] ]
         sp_unmerged = merged_sp[ ~merged_sp["Merged"] ]
         ur_unmerged = ur_only_df[ ~ur_only_df["Merged"] ]
-        merged_sp["Merged"]    = merged_sp["Merged"].fillna(False).astype(bool)
-        ur_only_df["Merged"]   = ur_only_df["Merged"].fillna(False).astype(bool)
+        merged_sp, ur_only_df = merge_ur_into_sp_opt(sp_df, ur_df)
+        merged_sp["Merged"]  = merged_sp["Merged"].fillna(False).astype(bool)
+        ur_only_df["Merged"] = ur_only_df["Merged"].fillna(False).astype(bool)
+
+        sp_merged   = merged_sp[ merged_sp["Merged"] ]
+        sp_unmerged = merged_sp[ ~merged_sp["Merged"] ]
+        ur_unmerged = ur_only_df[ ~ur_only_df["Merged"] ]
+
 
 
 
